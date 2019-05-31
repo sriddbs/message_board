7ecd57a2c3b3abe2f678631462096ed3885b086a
Sridhar Belagod <sriddbs.bs@gmail.com>
2019-06-01 01:35:21 +0530

    Add association b/w user and messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   validates :email, presence: true
+
+  has_many :messages
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:email) }
   end
+
+  describe 'associations' do
+    it { should have_many(:messages) }
+  end
 end
