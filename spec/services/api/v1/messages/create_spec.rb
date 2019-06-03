168e1fc867e80a21e57a9d4f58d41469a61e3c52
Sridhar Belagod <sriddbs.bs@gmail.com>
2019-06-03 17:38:46 +0530

    Add spec for create message service

---
+++ spec/services/api/v1/messages/create_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe 'Messages create service' do
+  describe Api::V1::Messages::Create do
+    let(:params) { { message: {} } }
+    subject { described_class.new(params[:message]) }
+
+    context 'invalid params' do
+      it 'fails to create a message' do
+        message = subject.call
+
+        expect(message).not_to be_valid
+        expect(message).not_to be_persisted
+      end
+    end
+
+    context 'valid params' do
+      let(:user) { create(:user) }
+
+      let(:params) {
+        {
+          message:
+          {
+            title: 'abc',
+            description: 'desc',
+            user_id: user.id
+          }
+        }
+      }
+
+      it 'creates a message' do
+        message = subject.call
+
+        expect(message).to be_valid
+        expect(message).to be_persisted
+      end
+    end
+  end
+end
