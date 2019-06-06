1698502c28a7aabc1c1d05eb7d92e30896cf4def
Sridhar Belagod <sriddbs.bs@gmail.com>
2019-06-07 01:28:00 +0530

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(email: 'abc@example.com', first_name: 'a', last_name: 'b')
